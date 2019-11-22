f82e0719b95ecaaefe172067d66e81832ac9a837
Even <teven@hksinc.com>
2019-11-22 11:51:50 -0600

    Make variable names consistent.

--- code/simpleand.v
+++ code/simpleand.v
@@ -1,6 +1,6 @@
 // logic op AND for 1 bit inputs
-module SIMPLE_AND(input A,B, output reg aANDb);
+module SIMPLE_AND(input A,B, output reg out);
 	always @(*) begin
-		aANDb <= A & B;
+		out <= A & B;
 	end
 endmodule
